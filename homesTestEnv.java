4919d65550c3e88b75cbc906d68a5d06f014f8d3
Venkatasubramanian <63840257+Lrvram94@users.noreply.github.com>
2021-05-20 22:12:14 +1200

    Update homesTestEnv.java

--- homesTestEnv.java
+++ homesTestEnv.java
@@ -1,5 +1,4 @@
 package intro;
-
 import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -43,7 +42,7 @@ public class homesTestEnv {
 		} 
 		else
 		{
-			System.out.println("The page is Incorrect");
+			System.out.println("The page opened is Incorrect");
 		}
 
 		String SearchResult4 = driver.findElement(By.xpath("/html[1]/body[1]/homes-root[1]/homes-map-page[1]/div[1]/homes-drawer[1]/div[1]/div[2]/h2[1]")).getText();
@@ -75,16 +74,18 @@ public class homesTestEnv {
 		Thread.sleep(5000);
 		String Expected_page5 = "https://ketu.homes-test.com/map/lower-hutt/petone?searchLoc";
 		String Actual_page5 = driver.getCurrentUrl();
-		if (Actual_page5.contains(Expected_page5)) {
+		if (Actual_page5.contains(Expected_page5)) 
+		{
 			System.out.println("The Correct page is opened for search result - Petone Test Environment");
 		} 
 		else 
 		{
-			System.out.println("The page is Incorrect");
+			System.out.println("The page opened is Incorrect");
 		}
 
 		String SearchResult5 = driver.findElement(By.xpath("/html[1]/body[1]/homes-root[1]/homes-map-page[1]/div[1]/homes-drawer[1]/div[1]/div[2]/h2[1]")).getText();
-		if (SearchResult5.contains("Petone")) {
+		if (SearchResult5.contains("Petone")) 
+		{
 			System.out.println("Correct search results displayed - Petone Test Environment");
 		} 
 		else 
@@ -93,6 +94,7 @@ public class homesTestEnv {
 		}
 
 		// Test Case 3 B - 45, Puru Cresent Test Environment
+		
 		driver.get("https://ketu.homes-test.com/");
 		driver.findElement(By.id("autocomplete-search")).sendKeys("45 Puru Crescent");
 		WebDriverWait wait6 = new WebDriverWait(driver, 5);
@@ -110,12 +112,13 @@ public class homesTestEnv {
 		Thread.sleep(5000);
 		String Expected_page6 = "https://ketu.homes-test.com/map/wellington/lyall-bay/puru-crescent/45?searchLoc=rhf%7BFyb%7Bi%60@";
 		String Actual_page6 = driver.getCurrentUrl();
-		if (Actual_page6.equalsIgnoreCase(Expected_page6)) {
+		if (Actual_page6.equalsIgnoreCase(Expected_page6)) 
+		{
 			System.out.println("The Correct page is opened for search results - Puru, Cresent Test Environment");
 		} 
 		else 
 		{
-			System.out.println("The page is Incorrect");
+			System.out.println("The page opened is Incorrect");
 		}
 		String SearchResult6 = driver.findElement(By.xpath("/html[1]/body[1]/homes-root[1]/homes-map-page[1]/div[1]/homes-drawer[1]/div[1]/div[2]/h2[1]")).getText();
 		if (SearchResult6.contains("45 Puru Crescent, Lyall Bay"))
